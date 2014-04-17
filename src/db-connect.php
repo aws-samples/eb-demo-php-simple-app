eafbbd0433a2fc757250adba4f48daa788b03239
Evan Brown <evandbrown@gmail.com>
2014-04-16 19:13:16 -0700

    Use getenv for env vars

--- src/db-connect.php
+++ src/db-connect.php
@@ -1,7 +1,7 @@
 <?php 
-  define('DB_NAME', $_SERVER['RDS_DB_NAME']);
-  define('DB_USER', $_SERVER['RDS_USERNAME']);
-  define('DB_PASSWORD', $_SERVER['RDS_PASSWORD']);
-  define('DB_HOST', $_SERVER['RDS_HOSTNAME']);
+  define('DB_NAME', getenv('RDS_DB_NAME'));
+  define('DB_USER', getenv('RDS_USERNAME'));
+  define('DB_PASSWORD', getenv('RDS_PASSWORD'));
+  define('DB_HOST', getenv('RDS_HOSTNAME'));
   define('DB_TABLE', 'urler');
 ?>

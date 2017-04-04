a7802d8fb594cb66b3e6e7b6ec5a4b8c4e48c316
Hank <hank@powersportsupport.com>
2017-04-04 15:14:33 -0700

    Stickler Test

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Simple PHP App</title>
+        <title>Fork Change</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="web/assets/css/bootstrap.min.css" rel="stylesheet">
         <style>body {margin-top: 40px; background-color: #333;}</style>

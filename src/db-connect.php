82305cf8cf827b129c98048d580dc303838b6748
Tiago Henriques <tiagohen@amazon.de>
2017-07-10 13:59:30 +0200

    Use 3306 as default if no RDS_PORT provided

--- src/db-connect.php
+++ src/db-connect.php
@@ -3,6 +3,6 @@
   define('DB_USER', $_SERVER['RDS_USERNAME']);
   define('DB_PASSWORD', $_SERVER['RDS_PASSWORD']);
   define('DB_HOST', $_SERVER['RDS_HOSTNAME']);
-  define('DB_PORT', $_SERVER['RDS_PORT']);
+  define('DB_PORT', $_SERVER['RDS_PORT'] ?? '3306');
   define('DB_TABLE', 'urler');
 ?>
